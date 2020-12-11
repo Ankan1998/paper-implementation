050d7058677cbbf54fd69f10017babc6c68be011
Ankan1998 <ankanroni3@gmail.com>
2020-12-11 22:38:32 +0530

    vgg16 updated

--- advanceDL/Classification/vgg16.py
+++ advanceDL/Classification/vgg16.py
@@ -1,2 +1,23 @@
 import torch 
 import torch.nn as nn
+
+def double_conv(in_c,out_c):
+    d_conv=nn.Sequential(
+          nn.Conv2d(in_c,out_c,kernel_size=3,padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(out_c,out_c,kernel_size=3,padding=1),
+          nn.ReLU(inplace=True)
+        )
+    return d_conv
+
+
+def triple_conv(in_c,out_c):
+    t_conv=nn.Sequential(
+          nn.Conv2d(in_c,out_c,kernel_size=3,padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(out_c,out_c,kernel_size=3,padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(out_c,out_c,kernel_size=3,padding=1),
+          nn.ReLU(inplace=True)
+        )
+    return t_conv
