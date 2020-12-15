e4c52175f6187b5e98c018912a5588aa245533c7
Ankan1998 <ankanroni3@gmail.com>
2020-12-15 20:22:02 +0530

    vgg19 updated

--- advanceDL/Classification/vgg19.py
+++ advanceDL/Classification/vgg19.py
@@ -25,3 +25,54 @@ def quadruple_conv(in_c,out_c):
           nn.ReLU(inplace=True)
         )
     return t_conv
+
+
+
+
+class vgg19(nn.Module):
+
+  def __init__(self,num_of_classes):
+    super().__init__()
+
+    self.num_of_classes=num_of_classes
+
+    # Conv Layer
+    self.mpool2d=nn.MaxPool2d(kernel_size=2, stride=2)
+    self.dconv1=double_conv(3,64)
+    self.dconv2=double_conv(64,128)
+    self.qconv1=quadruple_conv(128,256)
+    self.qconv2=quadruple_conv(256,512)
+    self.qconv3=quadruple_conv(512,512)
+
+    #Linear Layer
+    self.flat=nn.Flatten()
+    self.relu=nn.ReLU()
+    self.fc1=nn.Linear(7*7*512,4096)
+    self.fc2=nn.Linear(4096,4096)
+    self.last=nn.Linear(4096,self.num_of_classes)
+
+
+
+
+  def forward(self,x):
+    #print("start",x.shape)
+    x=self.dconv1(x)
+    x=self.mpool2d(x)
+    x=self.dconv2(x)
+    x=self.mpool2d(x)
+    x=self.qconv1(x)
+    x=self.mpool2d(x)
+    x=self.qconv2(x)
+    x=self.mpool2d(x)
+    x=self.qconv3(x)
+    x=self.mpool2d(x)
+    x=self.flat(x)
+    x=self.fc1(x)
+    x=self.relu(x)
+    x=self.fc2(x)
+    x=self.relu(x)
+    x=self.last(x)
+
+    #print("final",x.shape)
+
+    return x
