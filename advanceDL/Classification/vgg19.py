690c46ca268113e414dfdcd46fc4496af44c9a91
Ankan1998 <ankanroni3@gmail.com>
2020-12-15 20:18:44 +0530

    vgg19 updated

--- advanceDL/Classification/vgg19.py
+++ advanceDL/Classification/vgg19.py
@@ -13,9 +13,11 @@ def double_conv(in_c,out_c):
     return d_conv
 
 
-def triple_conv(in_c,out_c):
+def quadruple_conv(in_c,out_c):
     t_conv=nn.Sequential(
           nn.Conv2d(in_c,out_c,kernel_size=3,padding=1),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(out_c,out_c,kernel_size=3,padding=1),
           nn.ReLU(inplace=True),
           nn.Conv2d(out_c,out_c,kernel_size=3,padding=1),
           nn.ReLU(inplace=True),
